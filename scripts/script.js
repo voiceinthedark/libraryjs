ed9e38fb28c68bf4117e351da82fe210a126300d
voiceinthedark <darkrisingforce@gmail.com>
2021-11-23 19:38:32 +0200

    - Add function to extract data from library - Add initial table frontend

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,5 @@
+const bookTable = document.querySelector('#booktable');
+
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
@@ -11,5 +13,47 @@ Book.prototype.info = function () {
   }`;
 };
 
-const newBook = new Book('The Hobbit', 'J.R.R. Tolkien', 295, false);
-console.log(newBook.info());
+Book.prototype._fields = function () {
+    return {
+        title: this.title,
+        author: this.author,
+        pages: this.pages,
+        read: this.read,
+    }
+}
+
+let library = [
+  new Book('The Hobbit', 'J.R.R. Tolkien', 295, false),
+  new Book('Starsight', 'Brandon Sanderson', 325, false),
+  new Book(
+    'The 7 1/2 deaths of Evelynn Hardcastle',
+    'Stuart Turton',
+    429,
+    true
+  ),
+  new Book('Recursion', 'Blake Crouch', 329, true),
+  new Book('Rhythm of War', 'Brandon Sanderson', 1232, true),
+];
+// const newBook = new Book('The Hobbit', 'J.R.R. Tolkien', 295, false);
+// console.log(newBook.info());
+
+function addBookToLibrary() {
+    
+}
+
+function showLibrary(books) {
+    books.forEach((book) =>{
+        console.log(book.info());   
+        const trow = document.createElement('tr');
+        for(let field of Object.values(book._fields())){
+            const td = document.createElement('td');
+            td.textContent = field;
+            trow.appendChild(td);
+        }
+        bookTable.appendChild(trow);        
+    }); 
+
+}
+
+showLibrary(library);
+
